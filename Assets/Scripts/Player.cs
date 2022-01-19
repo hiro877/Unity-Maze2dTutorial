e8ad799faee501efaf96206809e123e718b2e97b
hiro877 <hiro.htm877@gmail.com>
2022-01-19 17:13:44 +0900

    add lighting player add homing Enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Experimental.Rendering.Universal;
+
 public class Player : MonoBehaviour
 {
     public int keys = 0;
@@ -10,10 +12,11 @@ public class Player : MonoBehaviour
     public Text keyAmount;
     public Text youwin;
     public GameObject door;
+    public Light2D playerlight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(playerlight.pointLightOuterRadius);
     }
 
     // Update is called once per frame
@@ -35,10 +38,45 @@ public class Player : MonoBehaviour
         {
             transform.Translate(0, -speed * Time.deltaTime, 0);
         }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(LightOuterGradually());
+        }
 
         if(keys == 3){
             Destroy(door);
         }
+    }
+
+    public void OnClick()
+    {
+        StartCoroutine(LightOuterGradually());
+    }
+
+    IEnumerator LightOuterGradually(){
+        int loopcount = 20;
+        float waitsecond = 0.05f;
+        float offsetScale = 5.0f / loopcount;
+
+        var cachedWait = new WaitForSeconds(waitsecond);  
+
+        // playerlight.pointLightOuterRadius = 2.0f;
+        for (int loop = 0; loop < loopcount; loop++)
+        {
+            // update radius
+            playerlight.pointLightOuterRadius += offsetScale;
+            yield return cachedWait;
+        }
+
+        yield return new WaitForSeconds(3);
+
+        for (int loop = 0; loop < loopcount; loop++)
+        {
+            // update radius
+            playerlight.pointLightOuterRadius -= offsetScale;
+            yield return cachedWait;
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
